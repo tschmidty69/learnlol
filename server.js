eb1f49cd1dbee0cf1598c8a4a5744eb853ad7f24
tschmidty69 <tschmidty@yahoo.com>
2017-10-08 14:26:58 -0400

    Various fixes

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var async = require('async');
 var util = require('util');
 var handlebars = require('handlebars');
 var jsonfile = require('jsonfile');
+var jsonQuery = require('json-query')
 
 var app_base = 'C:/Users/tschmidt/github/learnlol/';
-var lol_patch = '7.18.1';
+var lol_patch = '7.19.1';
 var lol_lang = 'en_us';
 
 var fs = require('fs');
-//var obj = JSON.parse(fs.readFileSync(app_base + 'dragontail-' + lol_patch + '/' + lol_patch + '/data/' + lol_lang + '/championFull.json', 'utf8'));
 
 app.engine('handlebars', exphbs({defaultLayout: app_base + '/views/layouts/main'}));
 
@@ -44,9 +44,10 @@ app.get('/', function(req, res) {
 });
 
 app.get('/search', function(req, res) {
+  var dd_champion = JSON.parse(fs.readFileSync(app_base + 'dragontail-' + lol_patch + '/' + lol_patch + '/data/' + lol_lang + '/champion.json', 'utf8'));
   var data = {};
-  data.participants = {}
-  var api_key = 'RGAPI-e04a77eb-22f1-41fd-a609-acfd44632597';
+  data.participants = []
+  var api_key = 'RGAPI-a335ccb0-2088-4d4b-afbe-2d1d70a5378d';
   var s_toSearch = req.query.summoner.toLowerCase();
   var region = req.query.region;
 
@@ -55,92 +56,90 @@ app.get('/search', function(req, res) {
 
     function(callback) {
       var URL = 'https://' + region + '.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + s_toSearch + '?api_key=' + api_key;
-      console.log(URL);
       request(URL, function(err, response, body) {
+        //console.log(body);
         if(!err && response.statusCode == 200) {
           var json = JSON.parse(body);
-          console.log(json);
+          //console.log(json);
           data.id = json.id;
           data.name = json.name;
-          console.log("Searched for summoner: " + json.name + ' in region ' + region)
+          console.log("Found summoner " + json.name + ' in region ' + region)
           // So here it worked and we move to next function
-          //callback(null, data);
+          callback(null, data);
         } else {
-          data.name=s_toSearch+ 'Summoner not currently in game';
+          data.name=s_toSearch+ ' not currently in game';
           console.log(data.name);
           console.log(err);
+          callback(null, data);
         }
-        callback(null, data);
+
       });
     },
 
     // Fetches list of participants
     function(data, callback) {
       var URL = 'https://' + region + '.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/' + data.id + '?api_key=' + api_key;
-      console.log(URL);
+      //console.log(URL);
       request(URL, function(err, response, body) {
         if(!err && response.statusCode == 200) {
           var summId = 0;
           var json = JSON.parse(body);
           //console.log(util.inspect(json, {showHidden: false, depth: null}));
           // Can use array since no async functions
+          //console.log(json);
           for( var c = 0; c < json['participants'].length; c++) {
             //console.log('c=' + c);
             data.participants[c] = {};
-            data.participants[c].summId = json['participants'][c].summonerId;
-            data.participants[c].summName = json['participants'][c].summonerName;
+            data.participants[c].summonerId = json['participants'][c].summonerId;
+            data.participants[c].summonerName = json['participants'][c].summonerName;
             // blue = 0; red = 1
             if(json['participants'][c].teamId == '100') { data.participants[c].teamId = 0; }
             else {  data.participants[c].teamId = 1; }
             data.participants[c].championId = json['participants'][c].championId;
-            console.log(json['participants'][c].summonerName + ':' + json['participants'][c].summonerId + ':' + json['participants'][c].championName)
+            //console.log(json['participants'][c].summonerName + ':' + json['participants'][c].summonerId + ':' + json['participants'][c].championId)
           }
           async.each(data.participants, function(participant, callback) {
-            console.log('name=' + participant.summName + ', id=' + participant.championId)
-/*
-            var champ_json;
-            var URL = 'https://' + region + '.api.riotgames.com/lol/static-data/v3/champions/' + participant.championId + '?locale=en_US&tags=passive&tags=spells&api_key=' + api_key;
-
-            //console.log(URL)
-            request(URL, function(err, response, body) {
-
-                champ_json = JSON.parse(body);
-                //console.log(util.inspect(champ_json, {showHidden: false, depth: null}));
-                participant.championInfo = champ_json;
-                //console.log(participant.summName + ' playing ' + champ_json.name);
-              } else {
-                console.error(body, err);
-              }
-              callback();
-            });
-*/
+            console.log('Summoner: ' + participant.summonerName + ', id=' + participant.championId)
+						//console.log('')
           }, function(err) {
             if (err) {
               console.log('static-data error:' + err);
             }
             else {
               console.log('Loops finished');
-              callback(null, data);
             }
           });
 
+          console.log('Loops finished');
           // We are done go to next function
-
+          callback(null, data);
           //console.log('data length=' + json['participants'].length)
         } else {
           console.log('getSpectatorGameInfo error: ' + err);
           callback(null, data);
         }
+
       });
+      //callback(null, data);
+
     },
     // Now here is where it gets tricky for me
-/*
+
     function(data, callback) {
-      console.log('data length=' + data.participants.length)
+      //console.log('data length=' + data.participants.length)
+      //console.log(util.inspect(data, {showHidden: false, depth: null}));
+
       for( var d = 0; d < data.participants.length; d++) {
-        var champ_json;
-        console.log(d, data.participants[d].championId);
-        var URL = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + data.participants[d].championId + '?api_key=' + api_key;
+        data.participants[d].championInfo = {}
+        //var champ_json;
+        //console.log(d, data.participants[d].summonerName, data.participants[d].championId);
+        //console.log(util.inspect(dd_champion, {showHidden: false, depth: null}));
+        var championId = jsonQuery('data[**][key=' + data.participants[d].championId + ']', {data: dd_champion}).value.id
+        var championInfo = JSON.parse(fs.readFileSync(app_base + 'dragontail-' + lol_patch + '/' + lol_patch + '/data/' + lol_lang + '/champion/' + championId +'.json', 'utf8'));
+        data.participants[d].championInfo = championInfo.data[championId]
+        //console.log(util.inspect(data.participants[d].championInfo, {showHidden: false, depth: 1}));
+
+        /*var URL = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + data.participants[d].championId + '?api_key=' + api_key;
         request(URL, function(err, response, body) {
           if(!err && response.statusCode == 200) {
             champ_json = JSON.parse(body);
@@ -150,15 +149,18 @@ app.get('/search', function(req, res) {
             console.log(err);
           }
         });
+        */
         //console.log('d=' + d);
-        console.log(util.inspect(champ_json, {showHidden: false, depth: null}));
-        data.participants[d].championName = champ_json.name;
-        //console.log(data.participants[d].championName);
-        callback(null, data);
+        //console.log(util.inspect(champ_json, {showHidden: false, depth: null}));
+        //data.participants[d].championName = champ_json.name;
+        console.log(d, data.participants[d].summonerName, data.participants[d].championInfo.name);
+        console.log(util.inspect(data.participants[d].championInfo.spells, {showHidden: false, depth: 1}));
+        //callback(null, data);
 
       }
+      callback(null, data);
     },
-*/
+
   ],
   // Final callback function which writes out page
   function(err, data) {
